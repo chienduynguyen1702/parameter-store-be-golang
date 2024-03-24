84ccbe893b305011a71a3323e7bca51941679696
EdutoOps-BD <eduto.ops@gmail.com>
2024-03-25 01:45:06 +0700

    remove read .env file

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 )
 
 func init() {
-	initializers.LoadEnvVariables()
+	// initializers.LoadEnvVariables()
 	db, err := initializers.ConnectDatabase() // return *gorm.DB
 	if err != nil {
 		log.Fatal("Failed to connect to database")
