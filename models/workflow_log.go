2149e1ed7c6608937fd839a53c97ae00e4016b4b
chiennd172002 <chiennd172002@gmail.com>
2024-04-24 03:42:34 +0700

    conmmit syntax

--- models/workflow_log.go
+++ models/workflow_log.go
@@ -8,7 +8,7 @@ import (
 
 type WorkflowLog struct {
 	gorm.Model
-	WorkflowID    uint       `json:"workflow_id"`
+	WorkflowID    uint      `json:"workflow_id"`
 	WorkflowRunId uint      `json:"workflow_run_id"`
 	AttemptNumber int       `json:"attempt_number"`
 	State         string    `json:"state"`
