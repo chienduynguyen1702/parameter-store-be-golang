8ecf5ded102794554cb1326ef08a5566f81dc3e6
chiennd172002 <chiennd172002@gmail.com>
2024-06-19 15:55:34 +0700

    setup test and env test

--- test/main_test.go
+++ test/main_test.go
@@ -10,13 +10,15 @@ import (
 func TestMain(t *testing.T) {
 	err := godotenv.Load("../.env.test")
 	if err != nil {
-		panic("Error loading .env.test file")
+		panic("Error loading .env.test file for test")
 	}
 	enableTest := os.Getenv("ENABLE_TEST")
-	if enableTest == "true" {
+	if enableTest != "true" {
+		t.Log("Test is disabled")
+
+	} else { // Test is enabled
 		t.Run("TestAdd", testAdd)
 		t.Run("TestSubtract", testSubtract)
-	} else {
-		t.Log("Test is disabled")
+
 	}
 }
